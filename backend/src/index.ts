88fe1d81a5ff10164f623ba43f39361ab8acf3e0
Aditya Bhave~ <adityabhave525@gmail.com>
2024-07-25 18:20:07 +0530

    Added signin and signup logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,75 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { jwt, sign } from 'hono/jwt'
+import * as bcrypt from 'bcryptjs'
 
-const app = new Hono()
+const saltRound = 10
+
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL: string,
+    JWT_SECRET: string
+  }
+}>()
+
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.post('/api/v1/user/signup', (c) =>{
-  return c.text('Signup route')
+app.post('/api/v1/user/signup', async(c) =>{
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL
+  }).$extends(withAccelerate())
+    const body = await c.req.json();
+    const hash = await bcrypt.hash(body.password, saltRound)
+    try{
+      const user = await prisma.user.create({
+        data:{
+          email: body.email,
+          password: hash
+        }
+      })
+      const jwt = await sign({id: user.id}, c.env.JWT_SECRET)
+      return c.text(jwt)
+    }catch(e){
+      c.status(403)
+      return c.json({error: 'Error while signing up'})
+    }
+
 })
 
-app.post('/api/v1/user/signin', (c) =>{
-  return c.text('Signin route')
+app.post('/api/v1/user/signin', async (c) =>{
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+  
+  const user = await prisma.user.findUnique({
+    where:{
+      email: body.email
+    }
+  })
+
+  
+  if(!user){
+    c.status(403);
+    return c.json({error: "user not found"})
+  }
+  
+  const isMatch = await bcrypt.compare(body.password, user.password)
+
+  if(isMatch){
+    const jwt = await sign({id: user.id}, c.env.JWT_SECRET)
+    return c.json({jwt});
+  }
+
+  c.status(403);
+  return c.json({error: "Password Wrong bro"})
+  
+  
 })
 
 app.post('/api/v1/blog', (c) =>{
