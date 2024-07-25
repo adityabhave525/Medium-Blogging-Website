1d3afd6192c32f855d8d853572760898a02d960b
Aditya Bhave~ <adityabhave525@gmail.com>
2024-07-25 13:43:03 +0530

    Added basic routing in backend/src/index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,4 +6,29 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.post('/api/v1/user/signup', (c) =>{
+  return c.text('Signup route')
+})
+
+app.post('/api/v1/user/signin', (c) =>{
+  return c.text('Signin route')
+})
+
+app.post('/api/v1/blog', (c) =>{
+  return c.text('Blog route')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Update blog route')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  const id = c.req.param("id")
+  return c.text(`Get blog with ${id}`)
+})
+
+app.get('/api/v1/blog/bulk', (c) => {
+  return c.text(`Get all blogs`)
+})
+
 export default app
